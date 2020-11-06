8d02adc686bfd709469cdd16eb4c14b2f8e8b92b
JM-Sully <jess.marie.sullivan@gmail.com>
2020-11-06 07:52:53 +0000

    moves business logic to controller

--- app/controllers/greeter_controller.rb
+++ app/controllers/greeter_controller.rb
@@ -1,5 +1,8 @@
 class GreeterController < ApplicationController
   def hello
+    random_names = ["Jess", "Cody", "Martha"]
+    @name = random_names.sample
+    @time = Time.now
   end
   def goodbye
   end
