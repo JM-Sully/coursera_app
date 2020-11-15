9fb18373044552e831a7f405253e1e269542dd1b
JM-Sully <jess.marie.sullivan@gmail.com>
2020-11-15 08:33:07 +0000

    fixes typo

--- app/models/country.rb
+++ app/models/country.rb
@@ -2,8 +2,8 @@ class Country
     include HTTParty
 
     default_options.update(verify: false) # Turn off SSL verification
-
     base_uri 'https://restcountries.eu/rest/v2/'
+    
     format :json
 
     def self.all
@@ -11,10 +11,10 @@ class Country
 
         countries.each do |country|
             country['borders'].map! do |country_code|
-                countries.find { |country| country['alpha3code'] = country_code } ['name']
+                countries.find { |country| country['alpha3Code'] == country_code } ['name']
             end
             country['languages'].map! { |language| language['name']}
-            #country['currencies'].map! { |currency| currency['name'] }
+            
         end
 
         countries
