a3171f17b9e5bc3b2528b185f13ed5020103abec
JM-Sully <jess.marie.sullivan@gmail.com>
2020-11-20 09:40:38 +0000

    deletes greeter controller

--- app/controllers/greeter_controller.rb
+++
@@ -1,9 +0,0 @@
-class GreeterController < ApplicationController
-  def hello
-    random_names = ["Jess", "Cody", "Martha"]
-    @name = random_names.sample
-    @time = Time.now
-  end
-  def goodbye
-  end
-end
